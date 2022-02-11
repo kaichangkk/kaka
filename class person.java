7dd0a71d0f66c75bb4deb0cac851873b2c5de6b4
kaichangkk <99412440+kaichangkk@users.noreply.github.com>
2022-02-11 20:44:29 +0700

    commit

---
+++ class person.java
@@ -0,0 +1,15 @@
+class person{
+public class person;
+ //Attribute
+    protected string name;
+    protected int age;
+//Method
+    private int getAge() {
+        public static viod printimformation() {
+            int age = getAge();
+            
+        }
+    }
+    
+    
+}
